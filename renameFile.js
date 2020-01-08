6dc1ca6afee7d58a57976fe79f9b700cb5c26dae
Campos <ecampos@itsc.uah.edu>
2020-01-08 15:38:55 -0600

    Add additional requirements as S3 buckets do not allow for renaming.

--- renameFile.js
+++ renameFile.js
@@ -1,1 +1,3 @@
 //TODO- Implement function to accept a path within an S3 bucket and rename the file 
+    //TODO- Copy file within S3 bucket with new name
+    //TODO- Remove old file from S3 bucket
