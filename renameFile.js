95999086ccbe16c9f199ecc3eef8e6a4d8d1dbd9
Campos <ecampos@itsc.uah.edu>
2020-01-09 10:11:21 -0600

    Fixed renaming issue where _renamed preceded file extension. Current method of parsing filename will not work for complex extensions such as .tar.gz

--- renameFile.js
+++ renameFile.js
@@ -1,42 +1,44 @@
-//TODO- Implement function to accept a path within an S3 bucket and rename the file 
-    //TODO- Copy file within S3 bucket with new name
-    //TODO- Remove old file from S3 bucket
-
 //dependencies
 const aws = require('aws-sdk');
 const s3 = new aws.S3();
+const path = require('path');
 
 //define S3 bucket name and file name
 //TODO- define filename from url parameter when adapted for API
 const  BUCKET = "eddie-tutorial";
 const FILENAME = "test.txt";
 
+//NOTE- This method of parsing a file name to seperate will not work for "complex" file extensions such as .tar.gz
+let filename = path.parse(FILENAME).name;
+let extension = path.parse(FILENAME).ext;
+let newName = filename + "_renamed" + extension;
+
 exports.handler = function (event, context, callback) {
     //Copy file
     s3.copyObject({
         CopySource: BUCKET + '/' + FILENAME,
         Bucket: BUCKET,
-        Key: FILENAME + "_renamed"
+        Key: newName
     }, function(err, data){
         if (err) {
             console.log("Error: " + err)
         } else {
             console.log("Successful Copy");
         }
-    });
-    callback(null, "Copy Finished!");
-
-
-    //Delete old file
-    s3.deleteObject({
-        Bucket: BUCKET,
-        Key: FILENAME
-    }, function(err, data) {
-        if (err) {
-            console.log("Error: " + err)
-        } else {
-            console.log("Successful Deletion");
-        }
-    });
-    callback(null, "Deletion Finished!");
+    })
+        .promise()                  //Avoids deleting file before copy
+        .then(() =>
+        
+        //Delete old file
+        s3.deleteObject({
+            Bucket: BUCKET,
+            Key: FILENAME
+        }, function(err, data) {
+            if (err) {
+                console.log("Error: " + err)
+            } else {
+                console.log("Successful Deletion");
+            }
+        }));
+        callback(null, "Finished!");
 };
