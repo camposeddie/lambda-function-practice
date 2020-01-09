81f4984302987c820888ae5f069282d28b632eeb
Campos <ecampos@itsc.uah.edu>
2020-01-09 12:39:31 -0600

    Updated file to use async/await instead of promise then. Updated file to use node js string interpolation rather than overloaded concatenation.

--- renameFile.js
+++ renameFile.js
@@ -11,34 +11,36 @@ const FILENAME = "test.txt";
 //NOTE- This method of parsing a file name to seperate will not work for "complex" file extensions such as .tar.gz
 let filename = path.parse(FILENAME).name;
 let extension = path.parse(FILENAME).ext;
-let newName = filename + "_renamed" + extension;
+let newName = `${filename}_renamed${extension}`;
 
-exports.handler = function (event, context, callback) {
-    //Copy file
-    s3.copyObject({
-        CopySource: BUCKET + '/' + FILENAME,
-        Bucket: BUCKET,
-        Key: newName
-    }, function(err, data){
-        if (err) {
-            console.log("Error: " + err)
-        } else {
-            console.log("Successful Copy");
-        }
-    })
-        .promise()                  //Avoids deleting file before copy
-        .then(() =>
+exports.handler = async function (event, context, callback) {
+    
+            //Copy file
+    await   s3.copyObject({ 
+                CopySource: `${BUCKET}/${FILENAME}`,
+                Bucket: BUCKET, 
+                Key: newName
+                }, function(err, data){
+                    if (err) {
+                        console.log(`Error: ${err}`);
+                    } else {
+                        console.log(`Successful Copy`);
+                    }
+                }
+            );
+
+            //Delete old file
+            s3.deleteObject({
+                Bucket: BUCKET,
+                Key: FILENAME
+                }, function(err, data) {
+                    if (err) {
+                        console.log(`Error: ${err}`);
+                    } else {
+                        console.log(`Successful Deletion`);
+                    }
+                }
+            );
         
-        //Delete old file
-        s3.deleteObject({
-            Bucket: BUCKET,
-            Key: FILENAME
-        }, function(err, data) {
-            if (err) {
-                console.log("Error: " + err)
-            } else {
-                console.log("Successful Deletion");
-            }
-        }));
-        callback(null, "Finished!");
+    callback(null, `Finished!`);
 };
